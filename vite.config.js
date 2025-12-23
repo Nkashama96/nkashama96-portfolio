521f63e302c3e8c3adb5e577f76904ed877515e8
Nkashama96 <147906315+Nkashama96@users.noreply.github.com>
2025-12-23 19:50:59 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: "/Nkashama96-portfolio",
 });
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react({
-//       babel: {
-//         plugins: [['babel-plugin-react-compiler']],
-//       },
-//     }),
-//   ],
-// })
